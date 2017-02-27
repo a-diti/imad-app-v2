6b934e82c22b7b5dfbf2d9a8bd9d3d4620a86948
Aditi Agrawal <a-diti@users.noreply.github.com>
2017-02-28 01:36:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,16 @@ element.innerHTML= 'something new';
 
 //move the image
 var img=document.getElementById('madi');
+var marginLeft = 0;
+function moveRight(){
+    
+    marginLeft = marginLeft+  5;
+      img.style.marginLeft = marginLeft +'px';
+}
+
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(marginRight,50);
+  
 
     
 };
