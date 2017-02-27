401668ec54a0b7c8fedb28a2e598892667c40abb
Aditi Agrawal <a-diti@users.noreply.github.com>
2017-02-28 01:21:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+//change html content
+var element=document.getElementById('main-text');
+element.innerHTML= 'something new';
+
+//move the image
+var img=document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100 px';
+    
+};
