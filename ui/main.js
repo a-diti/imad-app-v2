2f2eb8c2f071658b2129dfe6b08cc138fb981204
Aditi Agrawal <a-diti@users.noreply.github.com>
2017-03-03 22:56:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,10 +22,9 @@ button.onclick = function () {
   request.send(null);
     
 };
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit');
-submit.onclick = function(){
+submit.onclick = function () {
      var request = new XMLHttpRequest();
     
        request.onreadystatechange = function () {
@@ -44,6 +43,8 @@ submit.onclick = function(){
    }
  }
 };
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
  request.open('GET','http://a-diti.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
     
