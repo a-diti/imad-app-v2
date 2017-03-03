93c7c12cc54f0bcc42158cdaf1eab734cc2aa86e
Aditi Agrawal <a-diti@users.noreply.github.com>
2017-03-03 22:36:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,6 +101,16 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name',function(req, res){
+    var name = req.query.name;
+    
+    names.push(name);
+    res.send(JSON.stringify(names));
+    
+});
+
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -118,18 +128,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
  
  
-var names = [];
-app.get('/submit-name/:name',function(req, res){
-    var name = req.params.name;
-    
-    names.push(name);
-    res.send(JSON.stringify(names));
-    
-});
-
-
-
-app.get('/favicon.ico', function (req, res) {
+ app.get('/favicon.ico', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi1.png'));
 });
 
