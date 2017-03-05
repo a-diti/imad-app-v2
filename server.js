2ae001898de2b8b45c22fef090f794254dd83cc0
Aditi Agrawal <a-diti@users.noreply.github.com>
2017-03-05 23:21:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
+
+var config = {
+  host: 'db.imad.hasura-app.io',
+  port: '5432',
+  user: 'a-diti',
+  database: 'a-diti',
+   password: process.env.DB_PASSWORD
+};
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -94,6 +104,20 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new pool(config);
+app.get('/test-db', function (req, res) {
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else {
+            res.send(JSON.stringify(result));
+        }
+        
+    });
+  
+});
+
 
 var counter = 0;
 app.get('/counter', function (req, res) {
