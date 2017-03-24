38f7c94f8a5f7425655c1d3a521fac35db64239a
Aditi Agrawal <a-diti@users.noreply.github.com>
2017-03-24 22:35:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,11 +108,11 @@ app.get('/', function (req, res) {
 
 function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed('hex');
+    return hashed.toString('hex');
 }
 
 app.get('/hash/:input', function (req, res) {
-  var hashedString = hash(request.params.input, 'random-string');
+  var hashedString = hash(req.params.input, 'random-string');
   res.send(hashedString);
 });
 
